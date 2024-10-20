cf1e7a043cd2c9826564090be6a4dca313b20c72
sameemcodes <mmmohamedsameem@gmail.com>
2024-10-20 19:28:57 +0530

    removed cloud_server

--- central_edge_node/anonymized_data/views.py
+++
@@ -1,9 +0,0 @@
-# anonymized_data/views.py
-
-from rest_framework import viewsets, permissions
-from .models import AnonymizedPatientData
-from .serializers import AnonymizedPatientDataSerializer
-
-class AnonymizedPatientDataViewSet(viewsets.ModelViewSet):
-    queryset = AnonymizedPatientData.objects.all()
-    serializer_class = AnonymizedPatientDataSerializer
